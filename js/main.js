f7f5ef7829cc58a903c3a3caf84291a531c411ba
Borys <borys.baca@gmail.com>
2022-05-26 22:39:22 +0200

    items prepared

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const next = document.querySelector('#right-arrow');
+const prev = document.querySelector('#left-arrow');
+const frame = document.querySelector('.slider__frame');
+const slides = document.querySelectorAll('slider__slide')
