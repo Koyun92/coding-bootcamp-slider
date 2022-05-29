9844439dd1ef397b4a4de96400211f6d0382418b
Borys <borys.baca@gmail.com>
2022-05-29 22:29:48 +0200

    functions and backroun positions to second image added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 const next = document.querySelector('#right-arrow');
 const prev = document.querySelector('#left-arrow');
 const frame = document.querySelector('.slider__frame');
-const slides = document.querySelectorAll('slider__slide')
+const slides = document.querySelectorAll('slider__slide');
+
+
+let currentPosition = 0;
+
+
+const moveToRight = () => {
+    currentPosition++
+    frame.style.transform = "translateX(-100%)"
+}
+const moveToLeft = () => {
+    currentPosition--
+    frame.style.transform = "translateX(0)"
+}
+
+
+next.addEventListener('click', moveToRight);
+prev.addEventListener('click', moveToLeft);
