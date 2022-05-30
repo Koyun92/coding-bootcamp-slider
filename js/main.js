d477fee3ebdaaa254699127cf8ca470b6d00703b
Borys <borys.baca@gmail.com>
2022-05-30 22:53:42 +0200

    js finished

--- js/main.js
+++ js/main.js
@@ -1,21 +1,27 @@
-const next = document.querySelector('#right-arrow');
-const prev = document.querySelector('#left-arrow');
+const arrows = document.querySelectorAll('.slider__navigation-button');
 const frame = document.querySelector('.slider__frame');
-const slides = document.querySelectorAll('slider__slide');
-
-
+const slides = document.querySelectorAll('.slider__slide');
 let currentPosition = 0;
 
 
-const moveToRight = () => {
-    currentPosition++
-    frame.style.transform = "translateX(-100%)"
-}
-const moveToLeft = () => {
-    currentPosition--
-    frame.style.transform = "translateX(0)"
-}
+const moveTo = function () {
 
 
-next.addEventListener('click', moveToRight);
-prev.addEventListener('click', moveToLeft);
+    if (this.classList.contains('right-arrow')) {
+        if (currentPosition > (slides.length - 1) * -100 || currentPosition === 0) {
+            currentPosition += -100;
+
+            frame.style.transform = `translateX(${currentPosition}%)`
+        }
+    } else if (this.classList.contains('left-arrow')) {
+        if (currentPosition < 0) {
+            currentPosition += 100;
+            frame.style.transform = `translateX(${currentPosition}%)`;
+        }
+
+    }
+
+}
+arrows.forEach(arrow => {
+    arrow.addEventListener('click', moveTo)
+});
